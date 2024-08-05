2ff35380fc2a47f046e877a3f728bf173399ac06
Vikalp Rusia <vikalprusia@gmail.com>
2024-08-06 00:35:43 +0530

    add docs to client

--- crawler_client.py
+++ crawler_client.py
@@ -1,8 +1,12 @@
-import requests
+"""Client for sending request to server"""
 import argparse
 import sys
 
+import requests
+
+
 def main(url):
+    """sends request to server"""
     response = requests.post('http://localhost:8001/api/v1/crawl/', json={'url': url})
     if response.status_code == 200:
         print_sitemap(response.json(), url)
@@ -19,17 +23,24 @@ def main(url):
         print_error("Something went wrong, check the server/client logs")
         sys.exit(3)
 
+
 def print_sitemap(sitemap, root, indent=0):
+    """recursively prints sitemap"""
     print(' ' * indent + root)
     for link in sitemap.get(root, []):
         print_sitemap(sitemap, link, indent + 2)
 
+
 def print_errors(errors):
+    """prints errors"""
     for error_url, error in errors.items():
         print_error(f"{error_url} caused due to {error}")
 
+
 def print_error(message):
+    "prints error to stderr"
     print(message, file=sys.stderr)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Web Crawler Client')
