597e8f501c91a7f583da985c4dcd6bf3f5317e22
Vikalp Rusia <vikalprusia@gmail.com>
2024-08-06 00:06:45 +0530

    add filters

---
+++ app/filters/health_check_filter.py
@@ -0,0 +1,17 @@
+"""Custom logger"""
+import logging
+
+from constants import OP_PATH
+
+
+class HealthCheckFilter(logging.Filter):
+    # pylint: disable=too-few-public-methods
+    """Remove health apis logs"""
+
+    def filter(self, record):
+        """
+        ignore health api logging
+        :param record:
+        :return:
+        """
+        return record.__dict__.get(OP_PATH).find("health") == -1
---
+++ app/filters/op_filter.py
@@ -0,0 +1,30 @@
+"""Custom logger"""
+import logging
+
+from constants import OP_ID, OP_PATH
+from middlewares.uuid_middleware import get_key
+
+
+class OpFilter(logging.Filter):
+    # pylint: disable=too-few-public-methods
+    """Filter class to apply filer to health api"""
+
+    def filter(self, record):
+        """
+        ignore health api logging
+        :param record:
+        :return:
+        """
+        op_id = get_key(OP_ID)
+        op_path = get_key(OP_PATH)
+        if op_id:
+            record.__dict__.update(op_id=op_id)
+        else:
+            record.__dict__.setdefault(OP_ID, "???")
+
+        if op_path:
+            record.__dict__.update(op_path=op_path)
+        else:
+            record.__dict__.setdefault(OP_PATH, "???")
+
+        return True
