2a3b6c70ee990c991286a5ea4b5e608a0530e779
Vikalp Rusia <vikalprusia@gmail.com>
2024-08-05 23:12:38 +0530

    error handling in client

--- crawler_client.py
+++ crawler_client.py
@@ -1,24 +1,39 @@
-import aiohttp
-import asyncio
+import requests
 import argparse
+import sys
 
-
-async def main(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.post('http://localhost:8000/api/v1/crawl/', json={'url': url}) as response:
-            sitemap = await response.json()
-            print_sitemap(sitemap, url)
-
+def main(url):
+    response = requests.post('http://localhost:8001/api/v1/crawl/', json={'url': url})
+    if response.status_code == 200:
+        print_sitemap(response.json(), url)
+    elif response.status_code == 404:
+        print_error(f"Error in the URL: {url}")
+        sys.exit(1)
+    elif response.status_code == 207:
+        resp = response.json()
+        print_sitemap(resp.get("sitemap"), url)
+        print_error("----- ERROR ------")
+        print_errors(resp.get("errors"))
+        sys.exit(2)
+    else:
+        print_error("Something went wrong, check the server/client logs")
+        sys.exit(3)
 
 def print_sitemap(sitemap, root, indent=0):
     print(' ' * indent + root)
     for link in sitemap.get(root, []):
         print_sitemap(sitemap, link, indent + 2)
 
+def print_errors(errors):
+    for error_url, error in errors.items():
+        print_error(f"{error_url} caused due to {error}")
+
+def print_error(message):
+    print(message, file=sys.stderr)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Web Crawler Client')
     parser.add_argument('url', type=str, help='The URL to start crawling from')
     args = parser.parse_args()
 
-    asyncio.run(main(args.url))
+    main(args.url)
