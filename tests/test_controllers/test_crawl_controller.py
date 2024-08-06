ea130595d8d143a2a1a6f034e8d469472a9e5384
Vikalp Rusia <vikalprusia@gmail.com>
2024-08-06 16:32:24 +0530

    use test-html files

--- tests/test_controllers/test_crawl_controller.py
+++ tests/test_controllers/test_crawl_controller.py
@@ -1,5 +1,6 @@
 """Tests crawl"""
 import contextlib
+import os
 from unittest.mock import AsyncMock
 
 import pytest
@@ -20,85 +21,44 @@ class TestCrawlController:
         cls.crawl_controller = CrawlController()
 
     def test_extract_links(self):
-        test_str = """
-<!doctype html>
-<html lang="en">
-<head>
-  <script type="module" src="/@vite/client"></script>
-  <meta charset="utf-8">
-  <base href="/">
-  <meta content="width=device-width, initial-scale=1" name="viewport">
-  <link href="favicon.ico" rel="icon" type="image/x-icon">
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&amp;display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link rel="stylesheet" href="styles.css"></head>
-<body cds-theme="light" class="mat-typography">
-<a href="./testing"></a>
-<script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script></body>
-</html>
-        """
-        links = self.crawl_controller.extract_links(test_str, ".")
-        assert links == [
-            "/",
-            "https://fonts.googleapis.com/css2",
-            "https://fonts.googleapis.com/icon",
-            "testing",
-        ]
+        current_dir = os.path.dirname(__file__)
+        file_path = os.path.join(current_dir, "html", "file-1.html")
+        with open(file_path, mode="r", encoding="utf-8") as file:
+            test_str = file.read()
+            links = self.crawl_controller.extract_links(test_str, ".")
+            assert links == [
+                "/",
+                "https://fonts.googleapis.com/css2",
+                "https://fonts.googleapis.com/icon",
+                "testing",
+            ]
 
-        links = self.crawl_controller.extract_links(test_str, "www.foo.bar.com/")
-        assert links == [
-            "/",
-            "https://fonts.googleapis.com/css2",
-            "https://fonts.googleapis.com/icon",
-            "www.foo.bar.com/testing",
-        ]
+            links = self.crawl_controller.extract_links(test_str, "www.foo.bar.com/")
+            assert links == [
+                "/",
+                "https://fonts.googleapis.com/css2",
+                "https://fonts.googleapis.com/icon",
+                "www.foo.bar.com/testing",
+            ]
 
     @pytest.mark.asyncio
     async def test_crawl(self, mocker: MockerFixture):
+        # connect redis client
         await RedisHelper().connect()
+        current_dir = os.path.dirname(__file__)
+        file_path = os.path.join(current_dir, "html", "file-2.html")
+
         # Create mock responses for different URLs
         mock_response_1 = mocker.MagicMock()
         mock_response_1.status = 200
-        mock_response_1.text = AsyncMock(
-            return_value="""
-<!doctype html>
-<html lang="en">
-<head>
-  <script type="module" src="/@vite/client"></script>
-  <meta charset="utf-8">
-  <base href="/">
-  <meta content="width=device-width, initial-scale=1" name="viewport">
-  <link href="favicon.ico" rel="icon" type="image/x-icon">
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&amp;display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link rel="stylesheet" href="styles.css"></head>
-<body cds-theme="light" class="mat-typography">
-<a href="./test-2"></a>
-<script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script></body>
-</html>
-        """
-        )
+        with open(file_path, mode="r", encoding="utf-8") as file:
+            mock_response_1.text = AsyncMock(return_value=file.read())
 
+        file_path = os.path.join(current_dir, "html", "file-3.html")
         mock_response_2 = mocker.MagicMock()
         mock_response_2.status = 200
-        mock_response_2.text = AsyncMock(
-            return_value="""
-<!doctype html>
-<html lang="en">
-<head>
-  <script type="module" src="/@vite/client"></script>
-  <meta charset="utf-8">
-  <base href="/">
-  <meta content="width=device-width, initial-scale=1" name="viewport">
-  <link href="favicon.ico" rel="icon" type="image/x-icon">
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&amp;display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link rel="stylesheet" href="styles.css"></head>
-<body cds-theme="light" class="mat-typography">
-<script src="polyfills.js" type="module"></script><script src="main.js" type="module"></script></body>
-</html>
-        """
-        )
+        with open(file_path, mode="r", encoding="utf-8") as file:
+            mock_response_2.text = AsyncMock(return_value=file.read())
 
         @contextlib.asynccontextmanager
         async def mock_get_side_effect(url):
